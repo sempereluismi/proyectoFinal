7f9f7f8dfb28d96193d5c62d8935100c897a2717
luis miguel <sempereluismiguel@gmail.com>
2023-04-19 10:09:07 +0200

    first commit

---
+++ src/todoapp/TodoApp.java
@@ -0,0 +1,16 @@
+package todoapp;
+
+/**
+ *
+ * @author a22luismsg
+ */
+public class TodoApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
